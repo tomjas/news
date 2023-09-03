47e07cecbaf25c4d2db95f7858f8e4da7b1076f2
jasinskt <tomasz.jasinski@contractors.roche.com>
2023-09-03 19:25:23 +0200

    Added flyway Added datasource and flyway configuration

--- src/main/java/pl/mobilet/news/NewsApplication.java
+++ src/main/java/pl/mobilet/news/NewsApplication.java
@@ -1,13 +1,25 @@
 package pl.mobilet.news;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class NewsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewsApplication.class, args);
+	}
+
+	@Component
+	public static class BaselineOnMigrateMigrationStrategy implements FlywayMigrationStrategy {
+
+		@Override
+		public void migrate(Flyway flyway) {
+			flyway.migrate();
+		}
 	}
 
 }
